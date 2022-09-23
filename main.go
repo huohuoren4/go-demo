d0c48e142d2f6c174f363e2a3a9c337af76a2823
tom01 <674860357@qq.com>
2022-09-24 01:06:19 +0800

    增加了一些内容

--- main.go
+++ main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var i int = 123
-	var str string = "123"
-	var f float32 = 123.5646
-	var p *int = &i
-	fmt.Println(i, str)
-	fmt.Printf("hello, %T, %.2f, %d", str, f, *p)
+	//var flag bool= true
+	var flag01, flag02 bool = true, false
+	fmt.Println(flag01, flag02)
 }
