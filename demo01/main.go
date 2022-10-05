2d8eafdef4ff9021183b8ef9632eb963e06beb26
tom01 <674860357@qq.com>
2022-10-05 21:48:49 +0800

    增加 golang 代码

--- demo01/main.go
+++ demo01/main.go
@@ -4,8 +4,29 @@ import (
 	"fmt"
 )
 
+type Phone interface {
+	call() int
+}
+
+type ApplePhone struct {
+	phoneType string
+}
+
+type SumsungPhone struct {
+	phoneType string
+}
+
+func (iphone ApplePhone) call() int {
+	fmt.Println("This is a call from", iphone.phoneType)
+	return 1
+}
+
+func (sphone SumsungPhone) call() int {
+	fmt.Println("This is a call from", sphone.phoneType)
+	return 2
+}
+
 func main() {
-	if true {
-		fmt.Println("hello")
-	}
+	var phone Phone
+	fmt.Printf("%T", phone)
 }
