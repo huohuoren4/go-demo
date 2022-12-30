95b7776ce0dfa741255b776d99f17ac929b5a4e2
tom01 <674860357@qq.com>
2022-12-31 03:21:15 +0800

    修改go的结构和go的版本

--- demo01/main.go
+++ demo01/main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-type Phone interface {
-	call() int
-}
-type SumsungPhone struct {
-	phoneType string
-}
-
-func (sphone *SumsungPhone) call() int {
-	fmt.Println("This is a call from", sphone.phoneType)
-	return 2
-}
-
 func main() {
-	bufio.NewReader(os.Stdin)
+
 }
