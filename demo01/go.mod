bd1aa1594e508054afc9577eda857c833104f474
tom01 <674860357@qq.com>
2022-12-31 03:26:24 +0800

    增加新文件

--- demo01/go.mod
+++ demo01/go.mod
@@ -1,3 +1,5 @@
 module demo01
 
 go 1.14
+
+require github.com/spf13/viper v1.7.0
--- demo01/main.go
+++ demo01/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/spf13/viper" // 支持`json，toml，ini，yaml，hcl，env`等格式的文件内容
+	"path/filepath"
+)
 
+type YamlStruct struct {
+	Name    string `yaml:"name" json:"name"`
+	Age     int    `yaml:"age" json:"age"`
+	Address string `yaml:"address" json:"address"`
+}
+
+/**
+读配置文件
+@filename: 文件名
+@filetype: 文件类型, json, yaml等类型
+*/
+func ReadConfig(filename string) interface{} {
+	ext := filepath.Ext(filename)[1:]
+	viper.SetConfigType(ext)
+	viper.SetConfigFile(filename)
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	var yaml YamlStruct
+	err = viper.Unmarshal(&yaml)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	return yaml
+}
+
+func WriteConfig(filename string, content YamlStruct) {
+	ext := filepath.Ext(filename)[1:]
+	viper.SetConfigType(ext)
+	viper.SetConfigFile(filename)
+	viper.Set("name", content.Name)
+	viper.Set("age", content.Age)
+	viper.Set("address", content.Address)
+	err := viper.WriteConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func main() {
+	fmt.Println(ReadConfig("test.yaml"))
+	content := YamlStruct{Name: "tom", Age: 456, Address: "中关村晓坪路1203路"}
+	WriteConfig("test01.json", content)
 }
